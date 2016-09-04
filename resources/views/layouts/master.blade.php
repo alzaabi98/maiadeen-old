1d9c471cce742140fc5ffc9235126fc5904c6426
alzaabi98@gmail.com <alzaabi98@gmail.com>
2016-09-04 19:41:57 +0400

    s
    s

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,11 @@
 <head>
   <title>Slick Playground</title>
   <meta charset="UTF-8">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" >
+  <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" >
+    <link rel="stylesheet" href="assets/css/custom.css" >
+
+  <script src="assets/js/bootstrap.min.js" ></script>
 <link href="https://fonts.googleapis.com/css?family=Mirza:700" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="assets/slick/slick.css">
   <link rel="stylesheet" type="text/css" href="assets/slick/slick-theme.css">
