84948b951520e7906f904caa7f3d30bde27917e3
singhsukh <sukhwinder.webexpert@gmail.com>
2016-11-14 16:18:26 +0530

    changes done in createcourse view

--- resources/views/pages/createcourse.blade.php
+++ resources/views/pages/createcourse.blade.php
@@ -10,6 +10,9 @@
 
 @section('content')
 
+          
+              
+               
 		<div class="container">
 			<div class="row main">
 				
@@ -21,13 +24,28 @@
 							</div>
 							<div class="cols-sm-10">
 								
+								@if (Auth::check())
 								{!! Form::open(['route' => ['writer_path']]) !!}
+							     @else
+									 @section('javascript')
+								<script>
+								$("#hello").click(function(){
+								$("#popup").css("display","block");
+								});
+								</script>
+								
+								@stop
+								 
+							    @endif
+								
 								{!! Form::text('profession', '',array('required', 
 								'class'=>'form-control text inheight', 
 								'placeholder'=>'Enter the name of course')) !!}
-								{!! Form::submit('Create course', array('class' => ' form-group btn btn-warning btn-lg btn-block login-button')) !!}
+								{!! Form::submit('Create course', array('id'=>'hello','class' => 'crsbtn form-group btn btn-warning btn-lg btn-block login-button')) !!}
+							    <div id="popup" class="text" style="display:none">Please <a href="/login" style="color:#EB961E">login</a> first to create course</div>
 								{!! Form::close() !!}
 								
+						
 							</div>
 						</div>
 						</div>
@@ -36,3 +54,4 @@
 
 		
 @stop
+
